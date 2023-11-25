eec0869a365fe50f546c9a099be9889b7865dce7
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2023-11-24 23:34:38 -0500

    Add comment for timeout

--- src/network/axiosInstance.ts
+++ src/network/axiosInstance.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: 'https://restcountries.com/',
+    // The timeout option specifies the number of milliseconds before the request is aborted if the server does not respond.
     timeout: 5000,
 });
 
